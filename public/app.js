5ed09749cbffa17d930660e2b165ab9654a1549e
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-04-04 09:32:53 +0200

    merge

--- public/app.js
+++ public/app.js
@@ -1,17 +1,70 @@
-const htmlFooter = `
-<footer>
-</footer>
-`
+const resource = require('./public/resource.js')
 
-const htmlHeader = `
-<header>
-</header>
-`
+const mainDiv = document.getElementById('output')
+const render = html => {
+  mainDiv.innerHTML = html
+}
 
-const htmlProfilePage = `
+// controller object that has the routes for property
 
-`
+const controllers = {
 
-exports.htmlFooter = htmlFooter
-exports.htmlHeader = htmlHeader
-exports.htmlProfilePage = htmlProfilePage
+    // controller's property to render for root's route
+    '/': () => fetch('/')
+    .then(wilder => wilder.json())
+    .then(wilder => render(
+    `<div class="container">
+      <div class="jumbotron">
+        <h1 class="display-3">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="/about" role="button">Learn more »</a></p>
+        <p><a class="btn btn-success btn-lg" href="/users/new" role="button">Add a pirate »</a></p>
+      </div>
+      <div class="row">${wilder}</div>
+    </div>`))
+    
+
+    ,
+  
+    /*
+    '/users/:slug': ctx => {
+      const { slug } = ctx.params
+      fetch('/pirates')
+      .then(res => res.json())
+      .then(pirates => pirates.find(pirate => pirate.slug === slug))
+      .then(pirate => render(`<div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="${pirate.image}" alt="${pirate.firstName} ${pirate.lastName}" class="img-fluid" />
+          </div>
+          <div class="col-md-6">
+            <h1>${pirate.firstName} ${pirate.lastName}</h1>
+            <p>${pirate.bio}</p>
+          </div>
+        </div>
+      </div>`))
+    },
+  
+    '/about': () => render(
+      `<div class="container">
+        <section class="jumbotron text-center">
+          <h1 class="jumbotron-heading">About Us</h1>
+          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+          <a class="btn btn-primary btn-lg" href="/" role="button">Back to home page »</a>
+       </section>
+      </div>`
+    ), */
+  
+    '*': () => render('<h1>Not Found</h1>')
+  } // closing controllers object
+
+  (() => {
+
+    ['/', '/profile/:wilderName', '*'].forEach(
+      path => page(path, controllers[path])
+    )
+    page()
+    // route()
+  
+  })()
+  
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const resource = require('./public/resource.js')
-const app_modules = require('./public/app.js')
 const app = express()
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 
-let contenuAjout = ""
-
-const html = (contenuAjout) => `
+const html = `
 <!doctype html>
 <html lang="en">
   <head>
@@ -22,65 +18,30 @@ const html = (contenuAjout) => `
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   </head>
   <body>
-    
     <div id='output'>
-      ${contenuAjout}
+    
     </div>
-
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="contenu.js"></script>
+    <script src="page.js"></script>
+    <script src="app.js"></script>
   </body>
 </html>`
 
-// i will do something when receiving the homepage request
+// doing anything when receiving the profile
 app.get('/', (req, res) => {
-  const HtmlAjout = `
-  <div>HTML homepage</div>
-  `
-  res.send(html(HtmlAjout+htmlFooter))
+  res.send('./wilder.json')
   res.end()
 })
 
-app.get('/connect', (req, res) => {
-  res.send(html(`contenu HTML ajouté pour page connect`))
-  res.end()
-})
-
-app.get('/profil', (req, res) => {
-
-  res.send(html(app_modules.htmlHeader+app_modules.htmlProfilePage+app_modules.htmlFooter))
-  res.end()
-})
-
-app.get('/admin', (req, res) => {
-  res.send(html(`contenu HTML ajouté pour page admin`))
-  res.end()
-})
-
-app.get('/notif', (req, res) => {
-  res.send(html(``))
-  res.end()
-})
-
-app.get('/offre', (req, res) => {
-  res.send(html(``))
-  res.end()
-})
-
-app.get('/actu', (req, res) => {
-  res.send(html(``))
-  res.end()
-})
 
 // i'm sending the skeleton of the page (when i'm receiving any of the routes)
 app.get('*', (req, res) => {
-  res.send(html(``))
+  res.send(html)
   res.end()
 })
-
 
 app.listen(8080)
