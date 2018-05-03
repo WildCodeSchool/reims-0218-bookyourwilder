51dd7032dd63be1f848000186aa870d2c3c714a6
philippe tarte <tarteserviceinformatique@sfr.fr>
2018-05-03 18:12:35 +0200

    merge

--- public/app.js
+++ public/app.js
@@ -300,7 +300,7 @@ const controllers = {
       <p class="card-text" id="bioArea"></p>
       <div  id="divBtnReadMore" class="row justify-content-around">
         <button type="button" class="btn btn-primary">Read More</button>
-      /div>
+      </div>
     </div>
   </div>
   <div class="card mb-3" style="box-shadow: 1px 2px 10px rgba(0,0,0,0.5);background-color: rgb(255, 127, 43);">
@@ -360,76 +360,9 @@ const controllers = {
                       <label for="inputMail" class="col-12 col-sm-5">Mail</label>
                       <label for="inputMdp" class="col-12 col-sm-5">password</label>
                     </div>
-<<<<<<< HEAD
                     <div class="row justify-content-around">
                       <input maxlength="255" name="mail" type="text" class="form-control col-12 col-sm-5" id="inputMail" placeholder="${wilder.mail}">
                       <input name="password" type="text" class="form-control col-12 col-sm-5" id="inputMdp" placeholder="${wilder.password}">
-=======
-                    <div class="modal-body" id="editeur">
-                        <form id="changeProfile">
-                            <fieldset id="fsWilder">
-                                <input type="hidden" name="wilderChange_id" class="form-control">
-                                <fieldset class="form-group" id="nameWilder">
-                                    <div class="row justify-content-around">
-                                        <label for="inputFirstName" class="col-12 col-sm-5">First Name</label>
-                                        <label for="inputLastName" class="col-12 col-sm-5">Last name</label>
-                                    </div>
-                                    <div class="row justify-content-around">
-                                        <input maxlength="40" name="firstName" type="text" value="" class="form-control col-12 col-sm-5" id="inputFirstName" placeholder="${wilder.firstName}">
-                                        <input maxlength="40" name="lastName" type="text" class="form-control col-12 col-sm-5" id="inputLastName" placeholder="${wilder.lastName}">
-                                    </div>
-                                </fieldset>
-                                <fieldset class="form-group row justify-content-around">
-                                    <label for="inputTitle" class="col-11">Title</label>
-                                    <input maxlength="40" name="title" type="text" class="form-control col-11" id="inputTitle" placeholder="Titre de votre profil">
-                                </fieldset>
-                                <fieldset class="form-group row justify-content-around">
-                                    <label for="inputImageUrl" class="col-11">Image URL</label>
-                                    <input name="image" type="text" class="form-control col-11" id="inputImageUrl" placeholder="URL de votre avatar">
-                                </fieldset>
-                                <fieldset class="form-group row justify-content-around">
-                                    <label for="inputBio" class="col-11">Bio</label>
-                                    <textarea maxlength="500" name="bio" class="form-control col-11" id="txtBio" placeholder="Une description de vous. Pensez à l'actualiser régulièrement"></textarea>
-                                    <textarea maxlength="500" name="projet" class="form-control col-11" id="txtProjet" placeholder="Quels sont vos projets à venir ?"></textarea>
-                                    <textarea maxlength="500" name="coteWild" class="form-control col-11" id="txtWild" placeholder="Quel est le petit détail qui vous rend original ?"></textarea>
-                                </fieldset>
-                                <fieldset class="form-group">
-                                    <div class="row justify-content-around">
-                                        <label for="inputMail" class="col-12 col-sm-5">Mail</label>
-                                        <label for="inputMdp" class="col-12 col-sm-5">password</label>
-                                    </div>
-                                    <div class="row justify-content-around">
-                                        <input maxlength="255" name="mail" type="text" class="form-control col-12 col-sm-5" id="inputMail" placeholder="Nouveau mail">
-                                        <input name="password" type="text" class="form-control col-12 col-sm-5" id="inputMdp" placeholder="Nouveau mot de passe">
-                                    </div>
-                                </fieldset>
-                                <fieldset class="form-group" id="links">
-                                    <div class="row justify-content-around">
-                                        <label for="inputLinkedin" class="col-12 col-sm-5">Linkedin</label>
-                                        <label for="inputGithub" class="col-12 col-sm-5">Github</label>
-                                    </div>
-                                    <div class="row justify-content-around">
-                                        <input name="urlLi" type="text" class="form-control col-12 col-sm-5" id="inputLinkedin" placeholder="Lien LinkedIn">
-                                        <input name="urlGh" type="text" class="form-control col-12 col-sm-5" id="inputGithub" placeholder="Lien Github">
-                                    </div>
-                                </fieldset>
-                                <fieldset class="form-group" id="locations">
-                                    <div class="row justify-content-around">
-                                        <label for="inputAdress" class="col-12 col-sm-5">Adress</label>
-                                        <label for="inputMobility" class="col-12 col-sm-5">Mobility</label>
-                                    </div>
-                                    <div class="row justify-content-around">
-                                        <input name="adress" type="text" class="form-control col-12 col-sm-5" id="inputAdress" placeholder="Votre adresse">
-                                        <input name="mobility" type="text" class="form-control col-12 col-sm-5" id="inputMobility" placeholder="Mobilité géographique">
-                                    </div>
-                                </fieldset>
-                            </fieldset>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </form>
->>>>>>> e510f7c37d19e45be6d40a359607761b76919437
                     </div>
                   </fieldset>
                   <fieldset class="form-group" id="links">
